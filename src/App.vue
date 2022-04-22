b314da0a12437ae68c8f9e8be9bbfd2f5c0f7af1
Troy Bland <troybland@gmail.com>
2022-04-22 10:40:39 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="px-8 py-8 min-h-screen">
+    <ParentComponent/>
+  </div>
+</template>
+
+<script>
+import ParentComponent from "./components/ParentComponent";
+
+export default {
+  name: "App",
+  components: {
+    ParentComponent
+  }
+};
+</script>
+
+<style>
+body {
+  background: #43cea2;
+  background: linear-gradient(to bottom, #185a9d, #43cea2);
+}
+</style>
---
+++ src/repositories/gitHubRepository.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const baseDomain = "https://api.github.com";
+const baseURL = `${baseDomain}/graphql`;
+
+const Repository = axios.create({
+  baseURL,
+  headers: { Authorization: `Bearer yourSuppaToken` }
+});
+
+export default {
+  // Just FYI...
+  getRepo(owner = "octocat", name = "Hello-World") {
+    const query = `
+      repository(owner:"${owner}", name:"${name}) {
+        issues(last:20, states:CLOSED) {
+          edges {
+            node {
+              title
+              url
+              labels(first:5) {
+                edges {
+                  node {
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
+    return Repository.post("", { query });
+  }
+  // ...
+};
